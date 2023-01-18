4d2cae1764555e572f0bea29f834341f269011ce
lalithasri-30 <121622819+lalithasri-30@users.noreply.github.com>
2023-01-18 18:27:53 +0530

    #include<stdio.h> int main() {     int i;     scanf("%d",&i);     float c;     c=2.54*i;     printf("%0.2f",c); }

--- inches_to_centimeters.c
+++ inches_to_centimeters.c
@@ -1,14 +1,9 @@
 #include<stdio.h>
-void height(int i);
 int main()
 {
     int i;
     scanf("%d",&i);
-    height(i);
-}
-void height(int h)
-{
     float c;
-    c=2.54*h;
+    c=2.54*i;
     printf("%0.2f",c);
 }
